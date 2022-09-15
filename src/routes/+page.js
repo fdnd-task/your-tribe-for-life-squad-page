4499bbe2cb7ebc72ec42c2f9fb423bc73284568c
DphnZwp <zwuupdaphne@gmail.com>
2022-09-16 00:47:12 +0200

    ✨Added a working next button

---
+++ src/routes/+page.js
@@ -0,0 +1,8 @@
+// export async function load( { data, fetch }) {
+//   // const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//   // const users = await response.json()
+//   return {
+//     age: 28,
+//     // users: users
+//   }
+// }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const screenPhotos = [
+		'https://picsum.photos/300/200?random=1',
+		'https://picsum.photos/300/200?random=2',
+		'https://picsum.photos/300/200?random=3',
+		'https://picsum.photos/300/200?random=4'
+	]
+
+	let currentPhotoIndex = 0
+  
+  const next = () =>
+  currentPhotoIndex = (currentPhotoIndex + 1) % screenPhotos.length
+</script>
+
+{#each screenPhotos as photo, index}
+	{#if index == currentPhotoIndex}
+		<img src={photo} alt="" />
+	{/if}
+{/each}
+<button on:click={next}>Volgende</button>
+
+<style>
+  button {
+    padding: 1em;
+    background-color: grey;
+  }
+</style>
