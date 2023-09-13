4784caebd90564426b91ba9dd467021195700714
Tolga1999 <tolgadoganer@outlook.com>
2023-09-13 16:06:00 +0200

    starter project

---
+++ squad-page-rubber-hose/src/lib/slices/TitleContainer/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { PrismicRichText } from '@prismicio/svelte';
+
+	/** @type {import("@prismicio/client").Content.RichTextSlice} */
+	export let slice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<p>{slice.primary.websiteTitle}</p>
+</section>
+
+<style>
+</style>
