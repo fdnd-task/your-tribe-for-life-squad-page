0f90896ef0edd8483175f91df98ee7f3706e98c8
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-09-14 21:19:20 +0200

    start up

--- src/lib/slices/Header/index.svelte
+++ src/lib/slices/Header/index.svelte
@@ -10,3 +10,25 @@
   Placeholder component for {slice.slice_type} (variation: {slice.variation})
   Slices
 </section>
+
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="/"> logo </a>
+      </li>
+      <li>
+        <a href="/">Over ons</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+
+  header{
+    background-color: #49297E;
+    color: #fff;
+    padding: 5em;
+  }
+</style>
