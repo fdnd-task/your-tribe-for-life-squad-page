539279ce4b3d6dbded725dd9750e8835662b5867
Tolga1999 <tolgadoganer@outlook.com>
2023-09-18 17:58:42 +0200

    updated UID containers

--- src/lib/slices/MemberAboutContainer/index.svelte
+++ src/lib/slices/MemberAboutContainer/index.svelte
@@ -1,8 +1,5 @@
 <script>
 	/** @type {import("@prismicio/client").Content.MemberAboutContainerSlice} */
-	export let slice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
-</section>
+<!-- ga verder naar +page.svelte onder [uid] -->
--- src/routes/[uid]/+page.svelte
+++ src/routes/[uid]/+page.svelte
@@ -6,12 +6,11 @@
 	// what does export exactly do?
 	export let data;
 
-	console.log(data);
+	console.log(data.data1.slices);
 </script>
 
 <Header list={data.data2}/>
 <section>
 	<h1>{data.data1.title}</h1>
+	<p>{data.data1.slices[0].primary.heading_1}</p>
 </section>
-
-<!-- <SliceZone slices={data.slices} {components} /> -->
