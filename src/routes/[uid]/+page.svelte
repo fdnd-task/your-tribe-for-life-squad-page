b6877e2877a3aeb9113c80852810b819f42e1b40
SanHont <dehontsander@gmail.com>
2023-09-19 13:19:40 +0200

    styling

--- src/routes/[uid]/+page.svelte
+++ src/routes/[uid]/+page.svelte
@@ -12,7 +12,7 @@
 <Header list={data.data2} />
 
 <section>
-	<h1>{data.data1.slices[0].primary.heading_1}</h1>
+	<h1>{data.data1.slices[0].primary.heading_1}!</h1>
 
 	<img id="image" src={data.data1.image.url} alt="" />
 
@@ -24,7 +24,9 @@
 		<li>{data.data1.slices[0].primary.highlight_3}</li>
 	</ul>
 
-	<h2>{data.data1.slices[0].primary.heading_2}</h2>
+	<img id="pointy-finger" src={data.data1.slices[0].primary.pointy_finger.url} />
+
+	<h2 id="second-header">{data.data1.slices[0].primary.heading_2}</h2>
 
 	<p>{data.data1.slices[0].primary.paragraph_more_about}</p>
 
@@ -32,43 +34,56 @@
 </section>
 
 <style>
-	/* body {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		margin: 0;
-	} */
-
-	#image {
-		width: 10rem;
-		filter: saturate(0);
-		border: 1em solid #a8a8a8;
-	}
-
-	#image2 {
-		width: 10rem;
-		filter: saturate(0);
-	}
-
 	section {
 		text-align: center;
 		margin-left: 10%;
 		margin-right: 10%;
 	}
 
-	p, h2{
-		text-align: left; 
-		max-width: 600px; 
-		margin: 0 auto; 
-		padding: 20px; 
+	h1 {
+		margin-top: 3rem;
+		font-size: 2.5rem;
+		margin-bottom: 1rem;
 	}
-	
+
+	#image {
+		width: 17rem;
+		filter: saturate(0);
+		border: 1em solid #a8a8a8;
+	}
+
+	h2 {
+		margin-top: 1rem;
+		margin-bottom: 1.5rem;
+	}
+
 	ul {
 		padding: 0;
 		list-style: none;
 	}
+
 	li {
 		margin-bottom: 10px;
+		padding-left: 1rem;
+		text-align: left;
+		font-size: 1.3rem;
+		margin-bottom: 1.5rem;
+	}
+
+	#second-header {
+		text-align: left;
+		padding: 1rem;
+		margin-top: 2rem;
+		margin-bottom: -0.8rem;
+	}
+
+	p {
+		text-align: left;
+		padding: 1rem;
+	}
+
+	#image2 {
+		width: 18rem;
+		filter: saturate(0);
 	}
 </style>
