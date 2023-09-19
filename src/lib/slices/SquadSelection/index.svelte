a4edcc5d15895d7454beb2392cdb98ff386493a0
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-09-19 10:30:32 +0200

    push

--- src/lib/slices/SquadSelection/index.svelte
+++ src/lib/slices/SquadSelection/index.svelte
@@ -7,30 +7,68 @@
   data-slice-type={slice.slice_type}
   data-slice-variation={slice.variation}
 >
+
   <h1>{slice.primary.title}</h1>
+
+
+  <section class="subtitle">
   <p>{slice.primary.subtitle}</p>
-  <p>{slice.primary.squadA.url}</p>
-  <p>{slice.primary.squadB.url}</p>
+    </section>
+
+  <section class="button-a">
+    <a href={slice.primary.squadA.url} />
+    </section>
+    <section class="button-b"
+    <a href={slice.primary.squadA.url} />
+     </section>
+
+
 </section>
 
-<body>
-  
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0px;
+    line-height: 1.4;
+  }
 
-  <style>
-    h1 {
-      font-family: "Quantico", sans-serif;
-      text-transform: uppercase;
-      color: white;
-      -webkit-text-stroke-width: 2px;
-      -webkit-text-stroke-color: black;
-      transform: skew(-20deg) rotatez(2deg);
-      text-align: center;
-      margin-top: 21px;
-      margin-left: 18px;
-      position: absolute;
-      z-index: 100;
-      font-size: 50px;
-      text-shadow: 5px 1px var(--hot-pink);
-    }
-  </style>
-</body>
+  html {
+    height: 100vh;
+  }
+
+  :root {
+    --deep-blue: #55b6ae;
+    --blue-purple: #768BBA;
+    --pinkish: #d399bc;
+    --hot-pink: #ff3394;
+    --light-blue: #90dcff;
+  }
+
+  main {
+    margin-top: 100px;
+    width: 100%;
+    height: 100%;
+    margin-left: 10px;
+  }
+  h1 {
+    font-family: "Quantico", sans-serif;
+    text-transform: uppercase;
+    color: white;
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: black;
+    transform: skew(-20deg) rotatez(2deg);
+    text-align: center;
+    margin-top: 21px;
+    margin-left: 18px;
+    position: absolute;
+    z-index: 100;
+    font-size: 50px;
+    text-shadow: 5px 1px var(--hot-pink);
+  }
+
+
+
+
+
+  
+</style>
