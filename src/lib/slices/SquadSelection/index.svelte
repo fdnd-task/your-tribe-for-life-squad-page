562fe1bc1a4e5c18005ac31a17194d18f5ea2225
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-09-19 09:52:59 +0200

    select-pagina setup

--- src/lib/slices/SquadSelection/index.svelte
+++ src/lib/slices/SquadSelection/index.svelte
@@ -7,6 +7,30 @@
   data-slice-type={slice.slice_type}
   data-slice-variation={slice.variation}
 >
-  Placeholder component for {slice.slice_type} (variation: {slice.variation})
-  Slices
+  <h1>{slice.primary.title}</h1>
+  <p>{slice.primary.subtitle}</p>
+  <p>{slice.primary.squadA.url}</p>
+  <p>{slice.primary.squadB.url}</p>
 </section>
+
+<body>
+  
+
+  <style>
+    h1 {
+      font-family: "Quantico", sans-serif;
+      text-transform: uppercase;
+      color: white;
+      -webkit-text-stroke-width: 2px;
+      -webkit-text-stroke-color: black;
+      transform: skew(-20deg) rotatez(2deg);
+      text-align: center;
+      margin-top: 21px;
+      margin-left: 18px;
+      position: absolute;
+      z-index: 100;
+      font-size: 50px;
+      text-shadow: 5px 1px var(--hot-pink);
+    }
+  </style>
+</body>
