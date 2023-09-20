0320da02c5238f17908e6e8fd7c3af7df6c82009
Marwaxhello <marwahello33@gmail.com>
2023-09-20 15:28:27 +0200

    About

--- src/lib/slices/AboutContainer/index.svelte
+++ src/lib/slices/AboutContainer/index.svelte
@@ -1,8 +1,40 @@
 <script>
 	/** @type {import("@prismicio/client").Content.AboutContainerSlice} */
 	export let slice;
+	console.log(slice);
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+<section
+	class="about-container"
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
+<div class="grain"></div>
+	<!-- Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices -->
+
+	<h2>{slice.primary.about_title}</h2>
+	<p>{slice.primary.about_text}</p>
+	<img src={slice.primary.about_image.url} alt="hot dog gif" />
 </section>
+
+<style>
+	h2 {
+		/* margin-top: 1em;
+		margin-bottom: 1em; */
+		text-align: left;
+		padding: 2rem;
+	}
+
+	img {
+		width: 100%;
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+
+	p {
+		text-align: left;
+		margin-bottom: 50px;
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+</style>
