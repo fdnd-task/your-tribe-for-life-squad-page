04431bdccf3dcc4b5f76dfef4009c0ff0039bc18
Sascha-davidson <saschadavidson1@gmail.com>
2023-09-21 17:06:58 +0200

    mijn design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<script>
+    import global from '../global.css'
+</script>
+<slot /> 
--- src/routes/[squadUID]/[memberUID]/+page.svelte
+++ src/routes/[squadUID]/[memberUID]/+page.svelte
@@ -5,6 +5,34 @@
   export let data;
 </script>
 
-<a href="/">FDND Squadpage</a>
-<a href="/{squadpage}">{squadpage}</a>
-<h1>{data.page.first_name}</h1>
+<div class="container">
+  <div class="title-container">
+    <a href="/" class="title-link">FDND Squadpage</a>
+    <a href="/{squadpage}" class="title-link">{squadpage}</a>
+    <h1>{data.page.first_name} {data.page.last_name}</h1>
+  </div>
+  <div class="link-list">
+  </div>
+</div>
+
+<button class="back-to-top">test</button>
+
+<style>
+    /* deze min-hight zorgt er voor dat de tietle niet uit het scherm gaan. (screen breakpoint moeten apart gemaakt worden) */
+    /* text op 40px is in hoogte 46px zo voor elke title laag is het ++++ de hoogte van de title */
+    .title-container {
+    min-height: 228px;
+  }
+
+  @media only screen and (min-width: 382px) {
+    .title-container {
+      min-height: 184px;
+    }
+  }
+
+  @media only screen and (min-width: 418px) {
+    .title-container {
+      min-height: 138px;
+    }
+  }
+</style>
