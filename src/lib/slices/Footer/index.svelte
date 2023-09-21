bda527e017a8f9e9fc4febb58aa70e818d2e420d
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-09-21 10:47:53 +0200

    footer afgerond

--- src/lib/slices/Footer/index.svelte
+++ src/lib/slices/Footer/index.svelte
@@ -1,76 +1,73 @@
 <script>
   /** @type {import("@prismicio/client").Content.FooterSlice} */
+  import { PrismicImage } from '@prismicio/svelte'
   export let slice;
+
+  // console.log(slice.items[0].hvalogo);
+
 </script>
 
 <section
   data-slice-type={slice.slice_type}
   data-slice-variation={slice.variation}
 >
-  <!-- Placeholder component for {slice.slice_type} (variation: {slice.variation})
-  Slices -->
+  
+  <section>
+    <img class="hva-logo" src="{slice.items[0].hvalogo.url}" alt="hva logo"/>
+    <span> © 2023 FDND Squadpage </span>
+  </section>
+
 </section>
-
-
-  <footer>
-      <div class="footer-content">
-          <div class="footer-logo">
-              <img src="logo.png" alt="Logo">
-          </div>
-          <div class="footer-copyright">
-              &copy; 2023 Your Company Name
-          </div>
-      </div>
-  </footer>
 
 <style>
 
-/* Apply a background color and spacing to the footer */
-.wrapper {
-    min-height: 100vh; /* Ensure the content fills the entire viewport */
+  :root {
+    --white: #fff;
+    --turquoise: #55B6AE;
+  }
+
+  section{
+    background-color: var(--turquoise);
+    width: 100%;
+    height: auto;
+    margin-top: 2em;
+    position: absolute;
+    bottom: 0;
+
     display: flex;
     flex-direction: column;
-}
+    justify-content: center;
+    align-items: center;
+  }
 
-footer {
-    background-color: #333;
-    color: #fff;
-    padding: 40px 0;
-}
-
-/* Center the logo and copyright text on larger screens */
-.footer-content {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start; /* Align content to the top */
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 0 20px;
-}
-
-/* Style the logo */
-.footer-logo img {
-    max-width: 200px; /* Adjust the logo size */
+  .hva-logo{
+    max-width: 85%;
     height: auto;
-}
+  }
 
-/* Style the copyright text */
-.footer-copyright {
-    font-size: 14px;
-}
+  span{
+    color: var(--white);
+    font-size: .7em;
+    padding: 1em;
+  }
 
-/* Make the footer responsive for smaller screens */
-@media (max-width: 768px) {
-    .footer-content {
-        flex-direction: column;
-        align-items: center; /* Center content on small screens */
-        text-align: center;
+  @media (min-width: 400px) {
+
+    .hva-logo{
+    width: 15rem;
+    }
+  }
+
+  @media (min-width: 800px) {
+
+    section{
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .hva-logo{
+    width: 20rem;
     }
 
-    .footer-logo img {
-        margin-bottom: 20px; /* Add space below the logo on small screens */
-    }
-}
 
-
+  }
 </style>
