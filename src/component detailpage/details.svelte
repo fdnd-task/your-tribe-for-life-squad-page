c4fae8890501fc4109c0f9aa318a4369a07d2e8b
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-05 12:48:36 +0200

    test 2

---
+++ src/component detailpage/details.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { log } from 'degit/dist/index-688c5d50';
+	import { onMount } from 'svelte';
+	export let data;
+
+	if(data.people){
+		console.log("data.people", data.people)
+	}
+</script>
+
+
+{#if data.people}
+	<div class="details">
+
+
+	</div>
+	console.log(Data.people)
+	{:else}
+	<p>no content</p>
+	console.log("no")
+
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
+    import 
     
     // Check if the data has been received and is an array
-    console.log("Received data in +page.svelte:", data);
+    // console.log("Received data in +page.svelte:", data);
 </script>
 
 <!-- Only render if we have people in the data -->
@@ -17,4 +18,6 @@
     <p>No data available</p>
 {/if}
 
-<h2>123</h2>
+<h2>12234</h2>
+
+
