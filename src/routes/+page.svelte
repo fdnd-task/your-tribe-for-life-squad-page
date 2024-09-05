d8eff647705496538f753744f1b2a2472053a2c8
Remy Duivesteijn <remyd03@icloud.com>
2024-09-05 12:11:05 +0200

    directus connected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export let data
+</script>
+
+<main>
+    <p>lets go</p>
+</main>
+
+<style>
+</style>
