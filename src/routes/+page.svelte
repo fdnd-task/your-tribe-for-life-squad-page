ea362f0f607c50f86637b64c0b4f64c5b52e123f
lisagjh <lisagjhoorn@outlook.com>
2024-09-06 14:37:33 +0200

    start project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let data;
+  </script>
+  
+  <h1>squadpage</h1>
+  
+  <ul>
+    {#each data.persons as person}
+      <li>
+        {person.name}
+        {person.prefix}
+        {person.surname}
+      </li>
+    {/each}
+  </ul>
+  
+  
+  <style>
+      li {
+          list-style: "";
+      }
+  </style>
