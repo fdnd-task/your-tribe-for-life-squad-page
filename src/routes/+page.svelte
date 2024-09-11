547f33bfe73956d2fc36cb346466555395f3f2f2
Jason <jay.jpy.wong@gmail.com>
2024-09-11 18:00:26 +0200

    Feature : Project base HTML + CSS reset
    Map created in src for stylesheets and added workspace HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,36 @@
 <script>
 
 </script>
-<div class="lol">
-	<p>12345678</p>
-</div>
 
-<style>
-	div{
-		background-color: red;
-	}
-</style>
+<main>
+	<!--Workspace Jason-->
+	<section class="intro-section">
+		<div class="intro-text">
+			<!--H1 Welcome to our squadpage-->
+			<!--P-->
+		</div>
+		<!--Workspace Daan-->
+		<section class="random-cards">
+			<!--Random Cards for squadmembers component 5x-->
+		</section>
+	</section>
+	
+	<!--Workspace Annelinde-->
+	<section class="Meet our team">
+		<div class="about-us">
+			<!--H3-->
+		</div>
+		<div class="our-team-cards">
+			<!--Our team card components 6x-->
+		</div>
+	</section>
+
+	<!--Workspace Tristan-->
+	<section class="meet-our-tribe">
+		<!--H3-->
+		<div class="our-tribe-cards">
+			<!--Loop with card components & layout-->
+		</div>
+	</section>
+	
+</main>
