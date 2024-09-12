6489b1cf944ba55a9319ce4b2db066638ca95c0a
Daan Jacobs <daan@videotechniek.nl>
2024-09-12 12:42:00 +0200

    Card component
    I created a card component with a hover effect for the first section of our page

---
+++ src/lib/card-component.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let imgSrc; // Ontvang de imgSrc prop
+</script>
+
+<style>
+
+    img {
+        position: absolute;
+        border-radius: 1.2em;
+        filter: grayscale(100%); /* 100% maakt de afbeelding volledig zwart-wit */
+        transition: ease 1s; 
+        cursor: pointer;
+    }
+
+    img:hover {
+    transform: translateY(-15px); 
+    box-shadow: -5px 0 100px rgba(255, 255, 255, 0.5);
+    filter: grayscale(0);
+    scale: 1.02;
+}
+
+ 
+</style>
+
+<!-- Card Structure -->
+
+    <img class='image-card' src={imgSrc} alt="squad member" height="400" width="230">
+    
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
 <script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    // Check if the data has been received and is an array
+    console.log("Received data in +page.svelte:", data);
 
+
+    import CardComponent from "$lib/card-component.svelte";
+
+    // import Topbar from '../lib/Topbar.svelte';
+ 
+
+  
+    const imgSrcs = ['/images/2D2A8708-B2FC-418D-967C-F21EC6D2E020_1_102_o.jpeg'];
+    
 </script>
 
+<style>
 
+
+
+</style>
+{#each imgSrcs as imgSrc}
+        <CardComponent imgSrc={imgSrc}></CardComponent>
+    {/each}
+        
