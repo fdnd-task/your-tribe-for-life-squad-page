45a3ab60f70e4ca944e92dc2330c897f52161596
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-12 13:50:04 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,74 @@
 <script>
 
 </script>
-<!-- <div class="lol">
-	<p>12345678</p>
-</div> -->
-<section class="masonry masonry--h">
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #1"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #2"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #3"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #4"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #5"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #6"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #7"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #8"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #9"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #10"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #11"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #12"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #13"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #14"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #15"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #16"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #17"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #18"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #19"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #20"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #21"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #22"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #23"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #24"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #25"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #26"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #27"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #28"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #29"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #30"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #31"></figure>
-	<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #32"></figure>
-</section>
+
+<main>
+	<!--Workspace Jason-->
+	<section class="intro-section">
+		<div class="intro-text">
+			<!--H1 Welcome to our squadpage-->
+			<!--P-->
+		</div>
+		<!--Workspace Daan-->
+		<section class="random-cards">
+			<!--Random Cards for squadmembers component 5x-->
+		</section>
+	</section>
+	
+	<!--Workspace Annelinde-->
+	<section class="Meet our team">
+		<div class="about-us">
+			<!--H3-->
+		</div>
+		<div class="our-team-cards">
+			<!--Our team card components 6x-->
+		</div>
+	</section>
+
+	<!--Workspace Tristan-->
+	<section class="meet-our-tribe">
+		<!--H3-->
+		<div class="our-tribe-cards">
+			<!--Loop with card components & layout-->
+			<section class="masonry masonry--h">
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #1"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #2"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #3"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #4"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #5"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #6"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #7"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img class="masonry-img" alt="Masonry Brick #8"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #9"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #10"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #11"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #12"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #13"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #14"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #15"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #16"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #17"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #18"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #19"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #20"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #21"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #22"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #23"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #24"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #25"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #26"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #27"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #28"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #29"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #30"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #31"></figure>
+				<figure class="masonry-brick masonry-brick--h"><img  class="masonry-img" alt="Masonry Brick #32"></figure>
+			</section>
+			
+		</div>
+	</section>
+	
+</main>
 
 <style>
 *,
