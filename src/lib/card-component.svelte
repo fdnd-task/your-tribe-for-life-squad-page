8961542f52fdd384d6db3f88cacbc05234dbd9e9
Daan Jacobs <daan@videotechniek.nl>
2024-09-12 13:48:23 +0200

    random function
    i made a function that chooses 5 random links and put that in the imgsrc array.

--- src/lib/card-component.svelte
+++ src/lib/card-component.svelte
@@ -2,27 +2,31 @@
     export let imgSrc; // Ontvang de imgSrc prop
 </script>
 
-<style>
 
+
+
+
+<style>
     img {
-        position: absolute;
         border-radius: 1.2em;
-        filter: grayscale(100%); /* 100% maakt de afbeelding volledig zwart-wit */
-        transition: ease 1s; 
+        filter: grayscale(100%);
+        transition: filter 1s ease, transform 0.3s ease; /* Zachte overgang */
         cursor: pointer;
     }
 
     img:hover {
-    transform: translateY(-15px); 
-    box-shadow: -5px 0 100px rgba(255, 255, 255, 0.5);
-    filter: grayscale(0);
-    scale: 1.02;
-}
+        transform: translateY(-15px); /* Laat de afbeelding omhoog bewegen bij hover */
+        box-shadow: -5px 0 100px rgba(255, 255, 255, 0.5); /* Witte schaduw */
+        filter: grayscale(0); /* Kleur terugbrengen bij hover */
+        scale: 1.02; /* Beetje vergroten bij hover */
+    }
 
- 
 </style>
+
 
 <!-- Card Structure -->
 
     <img class='image-card' src={imgSrc} alt="squad member" height="400" width="230">
+
+
     
