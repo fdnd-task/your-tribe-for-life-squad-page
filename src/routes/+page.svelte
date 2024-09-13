b2fc2326840b5bfb2e4d16ba3f276ff666e7fb03
Jason <jay.jpy.wong@gmail.com>
2024-09-13 15:30:40 +0200

    Fix : Double HTML fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -134,28 +134,6 @@ const imgSrcs = getRandomLinks(links, 5);
 			<!--Our team card components 6x-->
 		</div>
 	</section>
-        <!--Workspace Jason-->
-        <section class="intro-section">
-            <div class="intro-text">
-                <!--H1 Welcome to our squadpage-->
-                <!--P-->
-            </div>
-            <!--Workspace Daan-->
-            <section class="random-cards">
-                <!--Random Cards for squadmembers component 5x-->
-              
-            </section>
-        </section>
-        
-        <!--Workspace Annelinde-->
-        <section class="Meet our team">
-            <div class="about-us">
-                <!--H3-->
-            </div>
-            <div class="our-team-cards">
-                <!--Our team card components 6x-->
-            </div>
-        </section>
     
       <!--Workspace Tristan-->
 	<section class="meet-our-tribe">
