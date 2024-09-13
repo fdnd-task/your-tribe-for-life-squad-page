dff204bd36b9b75c6c022597ce53d4da6fdfad2f
Jason <jay.jpy.wong@gmail.com>
2024-09-13 15:29:56 +0200

    Fix : Errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,54 +2,14 @@
 <script>
 // Stylesheets
 	import '../styles/grid.css';
-  import '../styles/random-card-component.css';
+  	import '../styles/random-card-component.css';
 
 	import '../styles/global.css'
 	import '../styles/intro-section.css'
 	import '../styles/fonts.css'
 
-</script>
-
-<main>
-	<!--Workspace Jason-->
-	<section class="intro-section">
-		<div class="intro-text">
-			<h1>Welcome to our Squadpage</h1>
-			<p>We are a group of students passionate about front-end development, exploring ways to broaden our skills and deepen our understanding of webdesign.</p>
-		</div>
-		<!--Workspace Daan-->
-		<section class="card-section-layout">
-			<!--Random Cards for squadmembers component 5x-->
-			<div class="div1">
-				<img src="../images/87DDE812-A203-49EE-A304-632056AF2DB5_4_5005_c.jpeg" alt="">
-			</div>
-			<div class="div2">
-				<img src="../images/6BED93D6-F18D-43B2-81E8-882FEB74D5F9_4_5005_c.jpeg" alt="">
-			</div>
-			<div class="div3">
-				<img src="../images/2D2A8708-B2FC-418D-967C-F21EC6D2E020_1_102_o.jpeg" alt="">
-			</div>
-			<div class="div4">
-				<img src="../images/1532C412-5E9A-452E-AE18-D87CE16968A2_4_5005_c.jpeg" alt="">
-			</div>
-			<div class="div5">
-				<img src="../images/3DDFB72A-4A72-4CE3-8C0F-B49A706288A7_4_5005_c.jpeg" alt="">
-			</div>
-		</section>
-	</section>
-	
-	<!--Workspace Annelinde-->
-	<section class="Meet our team">
-		<div class="about-us">
-			<!--H3-->
-		</div>
-		<div class="our-team-cards">
-			<!--Our team card components 6x-->
-		</div>
-	</section>
-
-// Components
-  	import CardComponent from "$lib/card-component.svelte";
+	// Components
+	import CardComponent from "$lib/card-component.svelte";
 
 // Variables
 	/** @type {import('./$types').PageData} */
@@ -128,16 +88,52 @@
             selectedLinks.push(selectedLink);
         }
     }
-
     // Geef de array met geselecteerde links terug
     return selectedLinks;
 }
 
 // Roep de functie aan om 5 willekeurige links te kiezen uit de 'links' array
 const imgSrcs = getRandomLinks(links, 5);
-</script>
-    
-    <main>
+
+</script> 
+
+<main>
+	<!--Workspace Jason-->
+	<section class="intro-section">
+		<div class="intro-text">
+			<h1>Welcome to our Squadpage</h1>
+			<p>We are a group of students passionate about front-end development, exploring ways to broaden our skills and deepen our understanding of webdesign.</p>
+		</div>
+		<!--Workspace Daan-->
+		<section class="card-section-layout">
+			<!--Random Cards for squadmembers component 5x-->
+			<div class="div1">
+				<!--Cardcomponent #1-->
+			</div>
+			<div class="div2">
+				<!--Cardcomponent #2-->
+			</div>
+			<div class="div3">
+				<!--Cardcomponent #3-->
+			</div>
+			<div class="div4">
+				<!--Cardcomponent #4-->
+			</div>
+			<div class="div5">
+				<!--Cardcomponent #5-->
+			</div>
+		</section>
+	</section>
+	
+	<!--Workspace Annelinde-->
+	<section class="Meet our team">
+		<div class="about-us">
+			<!--H3-->
+		</div>
+		<div class="our-team-cards">
+			<!--Our team card components 6x-->
+		</div>
+	</section>
         <!--Workspace Jason-->
         <section class="intro-section">
             <div class="intro-text">
@@ -147,9 +143,7 @@ const imgSrcs = getRandomLinks(links, 5);
             <!--Workspace Daan-->
             <section class="random-cards">
                 <!--Random Cards for squadmembers component 5x-->
-                {#each imgSrcs as imgSrc}
-        <CardComponent imgSrc={imgSrc}></CardComponent>
-    {/each}
+              
             </section>
         </section>
         
@@ -206,4 +200,3 @@ const imgSrcs = getRandomLinks(links, 5);
 		</div>
 	</section>
  </main>
-
