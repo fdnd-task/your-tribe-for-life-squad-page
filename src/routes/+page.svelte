25e49bea9c49c5d922dad6d5567b50f92bcd328c
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2024-09-20 12:19:33 +0200

    Meet the team sectie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -190,17 +190,6 @@
 				></CardComponent>
 			{/each}
 	</section>
-	
-	<!--Workspace Annelinde-->
-	<section class="meet-our-team">
-		<div class="about-us">
-			<h3>Meet our team</h3>
-			<p>Our team of frontend developers is a group of enthusiastic individuals focused on creating websites that are both user-friendly and visually appealing. We work well together to ensure everything runs smoothly, looks great, and is easy for users to understand.</p>
-		</div>
-		<div class="our-team-cards">
-			<!--Our team card components 6x-->
-		</div>
-	</section>
 
 	<!--Workspace Ellenoor-->
 	<section class="meet-our-team">
