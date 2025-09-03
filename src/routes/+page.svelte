67ee9a06129a49e1c1884d9c469b8f30b0087d41
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-03 22:11:45 +0200

    fix: name birthdate formatting #9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,25 @@
   const members = data.membersData
   console.log(members);
 
+  function calculateAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    // If birthday hasn't happened yet this year, subtract one
+    const birthdayHasPassed =
+      monthDiff > 0 || (monthDiff === 0 && dayDiff >= 0);
+
+    if (!birthdayHasPassed) {
+      age--;
+    }
+
+    return age;
+  }
   
 </script>
 
@@ -20,7 +39,7 @@
         <article class="person">
           <!-- <a href="/{member.id}">{member.name} </a> -->
           <p>{member.name}</p>
-          <p class="birthdate">{member.birthdate} Jaar</p>
+          <p class="birthdate">{calculateAge(member.birthdate)} Jaar</p>
           <a class="cross" href="">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g class="plus-outline"><g fill="#666" fill-rule="evenodd" class="Vector" clip-rule="evenodd"><path d="M12 3a1 1 0 0 1 1 1v16a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1"/><path d="M21 12a1 1 0 0 1-1 1H4a1 1 0 1 1 0-2h16a1 1 0 0 1 1 1"/></g></g></svg>
           </a>
