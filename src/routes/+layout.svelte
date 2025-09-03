07b773da02b59f97bafd82ad7f0901d919f05c07
Brancovanbeek <branco.van.beek@hva.nl>
2025-09-03 22:43:32 +0200

    feature/header, #16

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,24 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header>
+	<h1>Frontend Design & Development</h1>
+	<p>Squadpage 2025-2026</p>
+</header>
+
 {@render children?.()}
+
+<style>
+	header{
+		padding: clamp(10px, 5vw, 32px);
+
+		h1{
+			font-size: clamp(32px, 5vw, 96px);
+			margin-bottom: 12px;
+		}
+
+		p{
+			font-size: clamp(24px, 2vw, 32px);
+		}
+	}
+</style>
