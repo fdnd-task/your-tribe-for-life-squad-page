4a5abcccfcc855c2bc8799790eed37f31ffeaa66
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-03 14:48:28 +0200

    Les dorien toegepast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<!-- <script>
+    let { data } = $props();
+
+    const members = data.members.data
+    console.log(data.members);
+
+</script>
+
+{#each members as member}
+    <a href="/{member.id}">{member.name} - {member.bio}</a>
+{/each} -->
---
+++ src/routes/[id]/+page.server.js
@@ -0,0 +1,5 @@
+export async function load ({ url, params }) {
+    //hier komt het fetchen van data check oude server.js met alles
+
+    // return {member: membersData.data} // Return hier dus de data (Dit is geen render)
+}
