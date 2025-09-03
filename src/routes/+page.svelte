46bd5e4e99cb88d7368b1d2b678fb16275d69e1e
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-03 23:46:06 +0200

    #11: Background (En stylesheet) aangemaakt
    Ik heb de background gemaakt, zodat deze (als het goed is) op elke route hetzelfde moeten zijn, samen met de fonts. Dit is echter nog zonder animaties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<img src="src/lib/assets/pictures/logo.svg" alt="" class="logo" width="505" height="227">
+<h1>ONS TEAM</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 
