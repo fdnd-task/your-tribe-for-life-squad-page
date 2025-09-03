6598eb639d6fc4fe16a1b0732a2ebd4f210962b6
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-09-03 15:39:50 +0200

    Data opgehaald en getoont #7 #8 #9
    Data via database opgehaald en getoont

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 # **Team canvas**
 <img width="1934" height="1370" alt="image" src="https://github.com/user-attachments/assets/34048aeb-0d54-4d8e-ae53-6589b33f2fb0" />
 
@@ -9,5 +10,7 @@
 - Lastige code uitleggen in comments
 - Custom properties gebruiken
   
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 =======
+=======
+>>>>>>> Stashed changes
 <script>
     let { data } = $props();
 
@@ -13,5 +16,9 @@
 
 {#each members as member}
     <a href="/{member.id}">{member.name} - {member.id}</a>
+<<<<<<< Updated upstream
+{/each}
+>>>>>>> Stashed changes
+=======
 {/each}
 >>>>>>> Stashed changes
