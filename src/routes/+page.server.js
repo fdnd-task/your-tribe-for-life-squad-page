502bb61e0d8abeef1f9cb85aa640bf5835893695
MarsGotBars <marcinm@live.nl>
2025-09-03 22:41:54 +0200

    responsive image import

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,30 @@
-export async function load({url}){
-    const members = await fetch("https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202")
+export async function load({ url }) {
+  const members = await fetch(
+    "https://fdnd.directus.app/items/person?fields=name,profilecard,mugshot,birthdate&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202"
+  );
 
-    const {data: membersData} = await members.json();
+  const { data: membersData } = await members.json();
 
-    return {membersData}
+  // Loop om mugshot data te verkrijgen & aan de bestaande (membersData) te koppelen
+  // Wacht tot alle mugshot data verkregen is voordat we verdergaan
+  await Promise.all(
+    membersData.map(async (member) => {
+        // Fetch mugshot data
+      const mugshotResponse = await fetch(
+        `https://fdnd.directus.app/files/${member.mugshot}`
+      );
+
+      // Verwerk de mugshot data
+      const { data: mugshotData } = await mugshotResponse.json();
+
+      // Koppel de mugshot data aan de bestaande (membersData) als een object
+      member.mugshot = {
+        src: `https://fdnd.directus.app/assets/${member.mugshot}?height=320&quality=80`,
+        width: mugshotData?.width ?? null,
+        height: mugshotData?.height ?? null,
+      };
+    })
+  );
+
+  return { membersData };
 }
