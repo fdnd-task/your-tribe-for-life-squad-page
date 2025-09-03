b091a2b8249a8d725bd80bc5804fa23bc295651b
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-03 22:06:43 +0200

    feat: basic html #9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,29 @@
   
   const members = data.membersData
   console.log(members);
+
+  
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<main>
 
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
-
-{#each members as member }
-    <a href="/{member.id}">{member.name}</a>
-{/each}
+  <header>
+    <!-- Branco -->
+  </header>
+  
+  <section>
+    
+      {#each members as member }
+        <article class="person">
+          <!-- <a href="/{member.id}">{member.name} </a> -->
+          <p>{member.name}</p>
+          <p class="birthdate">{member.birthdate} Jaar</p>
+          <a class="cross" href="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g class="plus-outline"><g fill="#666" fill-rule="evenodd" class="Vector" clip-rule="evenodd"><path d="M12 3a1 1 0 0 1 1 1v16a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1"/><path d="M21 12a1 1 0 0 1-1 1H4a1 1 0 1 1 0-2h16a1 1 0 0 1 1 1"/></g></g></svg>
+          </a>
+          </article>
+      {/each}
+    
+  </section>
+  
+</main>
