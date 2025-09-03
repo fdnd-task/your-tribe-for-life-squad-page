8f36c83bc6a2760bb952fadd0d717c5e7b3df326
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-09-03 15:42:14 +0200

    fix github probleem #7 #8 #9

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 <script>
     let { data } = $props();
 
@@ -19,13 +9,4 @@
 
 {#each members as member}
     <a href="/{member.id}">{member.name} - {member.id}</a>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 {/each}
->>>>>>> Stashed changes
-=======
-{/each}
->>>>>>> Stashed changes
-=======
-{/each}
->>>>>>> Stashed changes
