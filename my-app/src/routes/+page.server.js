9cee3967ab1cffb0f514b503bd787019613ea3ac
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2025-09-04 11:27:37 +0200

    #15 filter aangepast
    [name][_eq]=2E achter huidige url geplakt.

--- my-app/src/routes/+page.server.js
+++ my-app/src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load({ url }) {
-    const members = await fetch("https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202");
+    const members = await fetch("https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E");
     const membersData = await members.json();
     console.log(membersData);
 
