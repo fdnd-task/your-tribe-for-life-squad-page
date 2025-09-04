36a09d60b9b543eea224f3a4a4754154a626ad01
julesbruins <jf-bruins@hotmail.com>
2025-09-04 12:07:07 +0200

    content detail inladen #9

--- my-app/src/routes/[id]/+page.svelte
+++ my-app/src/routes/[id]/+page.svelte
@@ -3,6 +3,18 @@
 
     const member = data.member; 
 </script>
+<main>
+    <a href="/">Terug naar home</a>
+        <h1>{member.name.split(' ')[0]}</h1>
+        <img src="https://fdnd.directus.app/assets/{member.mugshot}" alt="portret {member.name}">
 
-<a href="/">Terug naar home</a>
-<h1>Detailpagina van {member.name}</h1>
+        <ul>
+            <li>geboortedatum: {member.birthdate}</li>
+            <li>favoriete attribuut: {member.fav_attribute}</li>
+            <li>favoriete feature: {member.fav_feature}</li>
+            <li>favoriete property: {member.fav_property}</li>
+        </ul>
+        
+        <span>{member.fav_emoji}</span>
+</main>
+
