f4701c2f3157a8f4362f87084bc5e21f71f7113c
julesbruins <jf-bruins@hotmail.com>
2025-09-04 18:50:06 +0200

    1e versie responsive design #11

--- my-app/src/routes/[id]/+page.svelte
+++ my-app/src/routes/[id]/+page.svelte
@@ -4,28 +4,44 @@
     const member = data.member; 
 </script>
 <main>
+    <div class="content">
     <a href="/">Terug naar home</a>
         <h1 class="text-gradient">{member.name.split(' ')[0]}</h1>
         <img src="https://fdnd.directus.app/assets/{member.mugshot}" alt="portret {member.name}">
 
         <ul>
-            <li>geboortedatum: {member.birthdate}</li>
-            <li>favoriete attribuut: {member.fav_attribute}</li>
-            <li>favoriete feature: {member.fav_feature}</li>
-            <li>favoriete property: {member.fav_property}</li>
+            <li><span class="bold">geboortedatum:</span> {member.birthdate}</li>
+            <li><span class="bold">favoriete attribuut:</span> {member.fav_attribute}</li>
+            <li><span class="bold">favoriete feature:</span> {member.fav_feature}</li>
+            <li><span class="bold">favoriete property:</span> {member.fav_property}</li>
         </ul>
         
         <span><p>{member.fav_emoji}</p></span>
+    </div>
 </main>
 
 <style>
     main {
-        /* background-image: linear-gradient(150deg, var(--light-color-gradient), var(--dark-color-gradient)); */
         height: 100vh; 
-        /* background-color: var(--light-color-gradient); */
-        /* background-image: radial-gradient(var(--light-color-gradient), #7CC1C1); */
         background-image: url("/background-image.png");
+        background-size: cover;
     }
+
+    .content {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        /* @media (width > 550px) {
+            height: 100vh;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+    } */
+
+    }
+
 
     .text-gradient {
         background-image: linear-gradient(to top, var(--dark-color-gradient), var(--light-color-gradient));
@@ -34,24 +50,29 @@
         filter: drop-shadow(2px 2px #FFFFFF);
         
     }
+    .bold {
+        font-weight: bold;
+    }
 
     img {
         width: 80%;
-
     }
     ul {
         padding: 0;
+        margin-top: 3em;
     }
-    li, h1 {
-        color: var(--dark-color-gradient);
+    li {
+        color: var(--text-color);
     }
+
     span {
         p {
             font-size: 2em;
         }
     }
-
-       
+    a {
+        color: var(--text-color);
+    }
 
 
 
