93cee84393c313df7d98b246aba45e597b196e25
Sidopjescherm <hermussidney@gmail.com>
2025-09-04 17:09:47 +0200

    #13 Layout
    De basic layout van de polaroid pagina is hier te zien en laat zien hoe er twee delen zijn

--- my-app/src/routes/[id]/+page.svelte
+++ my-app/src/routes/[id]/+page.svelte
@@ -4,3 +4,24 @@
     // console.log(data);
     const student = data.student;
 </script>
+
+<a href="/">Back to flipbook</a>
+
+<section id="polaroid">
+    <article>
+        <img src="{student.avatar}" alt="{student.name}'s avatar" width="250px"/>
+        <h1>{student.name}</h1>
+    </article>
+
+    <article>
+        <h2>Details</h2>
+
+        <ul>
+            <li><a href="https://github.com/{student.github_handle}">{student.github_handle}</a></li>
+            <li>{student.fav_color}</li>
+            <li>{student.birthdate}</li>
+            <li>{student.fav_kitchen}</li>
+        </ul>
+    </article>
+</section>
+
