9cd0f3019142770d5da15b64c51bfe36a8d475b0
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-09-04 11:57:24 +0200

    fix: positioning fix and cleaner css #9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,14 @@
   const members = data.membersData
 </script>
 
-  
   <main class="person-container">
       {#each members as member, index }
         <details class="person">
            <summary>
-            <h2>{member.name}</h2>
-            <p class="birthdate">{member.age ? member.age + " Jaar" : "Leeftijd onbekend"}</p>
+              <div class="name-birthdate-container">
+                <h2>{member.name}</h2>
+                <p class="birthdate">{member.age ? member.age + " Jaar" : "Leeftijd onbekend"}</p>
+              </div>
             <a class="cross" href="https://github.com/{member.github_handle}" aria-label="Github link">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g class="plus-outline"><g fill="#666" fill-rule="evenodd" class="Vector" clip-rule="evenodd"><path d="M12 3a1 1 0 0 1 1 1v16a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1"/><path d="M21 12a1 1 0 0 1-1 1H4a1 1 0 1 1 0-2h16a1 1 0 0 1 1 1"/></g></g></svg>
             </a>
@@ -30,63 +31,95 @@
   </main>
 
 <style>
-    @import './App.css';
 
   :global(html, body) {
     height: 100%;
     background: black;
     margin: 0;
+    font-family: sans-serif;
+    font-size: 16px;
+
+    @media (min-width: 1000px) {
+      font-size: 18px;
+    }
   }
 
   .person-container {
-    display: flex;
-    flex-direction: column;
   }
 
   .person {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
     align-items: center;
-    padding: 2.25em;
-    position: relative;
 
-    @media (min-width: 1000px) {
-      grid-template-columns: 1fr 1fr 2.5fr;
+    padding-block: 0.63em;
+    margin-inline: 0.63rem;
+    
+    border-bottom: 1px solid #252525;
+
+    @media (min-width: 800px) {
+      padding-block: 2.25em;
+      margin-inline: 2.25em;
+    }
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    summary {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      cursor: pointer;
+
+      h2 {
+        margin: 0;
+        color: white;
+        font-size: 2rem;
+        overflow: hidden;
+      }
+
+      .birthdate {
+        margin: 0;
+        font-size: 1.25rem;
+        color: white;
+      }
+
+      .cross {
+        display: flex;
+        align-items: center;
+      }
+    }
+    
+    article {
+      margin-top: 2.25rem;
+      
+      picture {
+        display: inline-block;
+        width: 100%;
+        max-width: 32rem;
+
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+
+  .name-birthdate-container {
+    flex-grow: 1;
+    display: block;
+
+    @media (min-width: 800px) {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+
+    h2 {
     }
 
     p {
-      font-size: 24px;
-      color: white;
-      margin: 0;
-
-      @media (min-width: 600px) {
-        font-size: 32px;
-      }
-
-      &.birthdate {
-        font-size: 20px;
-        text-align: right;
-      }
-    }
-
-    .cross {
-      justify-self: end;
-      display: flex;
-      justify-items: center;
-    }
-
-    &::after {
-      content: '';
-      position: absolute;
-      left: 2.25em;
-      right: 2.25em;
-      bottom: 0;
-      height: 1px;
-      background: rgb(37, 37, 37);
-    }
-
-    &:last-child::after {
-      display: none;
     }
   }
+
+
 </style>
