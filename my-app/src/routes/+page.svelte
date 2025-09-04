9b7e256ec04b74576a9f438d104d1d87151557ed
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-09-04 13:53:05 +0200

    feature:homepage content toegevoegd en styling one-column-layout.
    Ik heb wat nieuwe dingen voor de homepage toegevoegd en die ook gestyled voor de one-column-layout.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,6 +4,76 @@
     const members = data.members; 
 </script>
 
-{#each members as member}
-    <a href="/{member.id}">{member.name}</a>
-{/each}
+<main>
+    <h2>SQUAD 2E</h2>
+
+    <ul>
+    {#each members as member}
+        <li><a href="/{member.id}">{member.name}</a></li>
+    {/each}
+    </ul>
+
+    <h1>SQUADPAGE</h1>
+
+    <ul>
+    {#each members as member}
+        <li><a href="/{member.id}">{member.name}</a></li>
+    {/each} 
+    </ul>
+        
+    <h2>SQUAD 2F</h2>
+</main>
+    
+        
+<style>
+    main {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-image: linear-gradient(to right bottom, #D3FFE5, #257070 );
+    }
+
+    h1 {
+        margin: 1em;
+        padding: 0;
+        text-align: center;
+    }
+
+    h2 {
+        margin: 0;
+        padding: 0;
+    }   
+
+    h2:nth-of-type(1) {
+        position: absolute;
+        left: 50%;
+        top: 10%;
+        transform: translateX(-50%);
+        text-align: center;
+    }
+
+    h2:nth-of-type(2) {
+        position: absolute;
+        left: 50%;
+        bottom: 10%;
+        transform: translateX(-50%);
+        text-align: center;
+    }
+    
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        gap: 2em;
+        & li {
+            background-color: white;
+            padding: 2em;
+        }
+    }
+        
+</style>
+    
