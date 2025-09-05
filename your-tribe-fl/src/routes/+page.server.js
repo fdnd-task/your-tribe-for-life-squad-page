84a14beee6604e024baab09a022479d12311649f
Recep <recep.yildiz@hva.nl>
2025-09-05 02:22:24 +0200

    #4 Squadpage api data fetching

--- your-tribe-fl/src/routes/+page.server.js
+++ your-tribe-fl/src/routes/+page.server.js
@@ -1,8 +1,12 @@
 export async function load({url}) {
-    const membersResposnse = await fetch('https://fdnd.directus.app/items/person/?filter[name][_starts_with]=R')
+    const membersResposnse = await fetch('https://fdnd.directus.app/items/squad?sort=name&fields=persons.*,persons.person_id.*&filter[cohort][_eq]=2526')
     const membersData = await membersResposnse.json();
 
+    const members = membersData.data.flatMap(squad =>
+        squad.persons.map(p => p.person_id)
+    );
+
     return {
-        members: membersData.data
+        members
     };
 }
