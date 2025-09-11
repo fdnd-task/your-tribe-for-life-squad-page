c6d4917e718735c674e89d7fe410388559821e0f
Recep <recep.yildiz@hva.nl>
2025-09-12 01:18:46 +0200

    #27 Made the HTML Prototype
    HTML Prototype and deleted some unused old code

--- your-tribe-fl/src/routes/+page.svelte
+++ your-tribe-fl/src/routes/+page.svelte
@@ -1,67 +1,20 @@
-
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-
 <script>
   let { data } = $props();
   const members = data.members;
 </script>
 
+<h2>Meet the student</h2>
+
+<form>
+    <select name="sort" id="sort">
+        <option value="default">Squad 2E</option>
+        <option value="squad2h">Squad 2H</option>
+    </select>
+</form>
+
 {#each members as member}
     <div class="profile-card">
-        <img class="profile-pic"
-        src={`https://fdnd.directus.app/assets/${member.mugshot}`}
-        alt={member.name}
-        width=200
-      />
-        <br>
-        <a class="profile-name" href="/{member.id}">{member.name}</a>
+        <img src="{`https://fdnd.directus.app/assets/${member.mugshot}`}" width="200" alt="student-{member.name}">
+        <a href="/">{member.name}</a>
     </div>
 {/each}
-
-
-<style>
-    .profile-card {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin: 2rem;
-    }
-    .profile-pic {
-        padding: 1rem;
-        margin-bottom: 1rem;
-        background: rgba(255, 255, 255, 0.17);
-        border-radius: 16px;
-        box-shadow: 
-        0 8px 32px rgba(0, 0, 0, 0.1),
-        inset 0 1px 0 rgba(255, 255, 255, 0.5),
-        inset 0 -1px 0 rgba(255, 255, 255, 0.1),
-        inset 0 0 0px 0px rgba(255, 255, 255, 0);
-        backdrop-filter: blur(7.5px);
-        -webkit-backdrop-filter: blur(7.5px);
-        border: 1px solid rgba(255, 255, 255, 1);
-    }
-    .profile-name {
-        text-decoration: none;
-        color: black;
-        font-weight: bold;
-        font-family: Arial, Helvetica, sans-serif;
-        padding: 0.75rem;
-        background: rgba(255, 255, 255, 0.17);
-        border-radius: 24px;
-        box-shadow: 
-        0 8px 32px rgba(0, 0, 0, 0.1),
-        inset 0 1px 0 rgba(255, 255, 255, 0.5),
-        inset 0 -1px 0 rgba(255, 255, 255, 0.1),
-        inset 0 0 0px 0px rgba(255, 255, 255, 0);
-        backdrop-filter: blur(7.5px);
-        -webkit-backdrop-filter: blur(7.5px);
-        border: 1px solid rgba(255, 255, 255, 1);
-        transition: transform 0.2s ease-in-out;
-        &:hover {
-            transform: scale(1.1) rotate(10deg);
-        }
-    }
-</style>
-
-
