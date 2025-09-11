6aba18260f98cdf5c1185cbe8004497b85718c7f
Karima002 <karima.mouadi@hva.nl>
2025-09-12 00:29:50 +0200

    #4 #19 feat: CSS squad2F

--- squadpage/src/routes/squad2f/+page.svelte
+++ squadpage/src/routes/squad2f/+page.svelte
@@ -36,3 +36,85 @@
         {/each}
         </ul>
 </main>
+
+
+<style>
+:root {
+--background-color: #EDE9FE;
+--card-color: #ffffff;
+    --text-color: black;
+    --font-regular: 'Jacques Francois', sans-serif;
+    --font-size-xsmall: 1em;
+    --font-size-small: 1.25em;
+    --font-size-medium: 1.75em;
+    --font-size-large: 3em;
+    background-color: var(--background-color);
+
+}
+
+@font-face {
+    font-family: 'Jacques Francois';
+    src: url('src/lib/fonts/JacquesFrancois-Regular.ttf') format('truetype');
+}
+
+
+* {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    padding: 0;
+    margin: 0;
+}
+
+a{
+    text-decoration: none;
+    color: var(--text-color);
+}
+
+.squadpage {
+    position: relative; /* nodig voor absolute positioning van menu-button */
+    min-height: 100vh;
+    padding: clamp(1rem, 4vw, 2rem);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: clamp(1rem, 2vw, 2rem);
+    background-color: var(--background-color);
+}
+
+
+.students-grid {
+    width: 100%;
+    max-width: 1100px;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: grid;
+    gap: clamp(1rem, 2.5vw, 2rem);
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+}
+
+.card {
+    background: var(--card-color);
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
+    display: grid;
+    gap: .75rem;
+    }
+
+.avatar img {
+    display: block;
+    width: 100%;
+    aspect-ratio: 3 / 4;
+    object-fit: cover;
+    border-radius: 8px;
+}
+
+.name {
+    margin: 0;
+    font-size: var(--font-size-small);
+    font-family: var(--font-regular);
+}
+</style>
+
