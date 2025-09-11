d7a0da8e7d502a8a28abd9faa4c514432b157157
Karima002 <karima.mouadi@hva.nl>
2025-09-12 00:17:51 +0200

    #4 #19 links to squads

--- squadpage/src/routes/+page.svelte
+++ squadpage/src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import menuSvg from '$lib/assets/menu.svg'; // menu icon
     import Header from '$lib/components/Header.svelte';
     import avatar from '$lib/assets/default-avatar.png';
+    import squad2e from '$lib/assets/squad2e.svg';
+    import squad2f from '$lib/assets/squad2f.svg';
 
     // Data uit +page.server.js
     let { data } = $props();
@@ -11,6 +13,16 @@
 
 <Header />
 <main class="squadpage">
+
+  <div class="squadpage-classes">
+      <a href="/squad2e">
+          <img src={squad2e} alt="squad 2E icon" width="175" height="auto">
+      </a>
+      <a href="/squad2f">
+          <img src={squad2f} alt="squad 2F icon" width="175" height="auto">
+      </a>
+  </div>
+
   <ul class="students-grid">
       {#each members as member}
         <li>
