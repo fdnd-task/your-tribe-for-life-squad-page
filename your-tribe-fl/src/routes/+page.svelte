297c7b35b6303961097c5b1186dfd495102310a9
Recep <recep.yildiz@hva.nl>
2025-09-12 10:52:03 +0200

    #28 created first one column layout

--- your-tribe-fl/src/routes/+page.svelte
+++ your-tribe-fl/src/routes/+page.svelte
@@ -3,18 +3,24 @@
   const members = data.members;
 </script>
 
-<h2>Meet the student</h2>
+<section class="home-heading">
+    <h2>Meet the members</h2>
+</section>
 
-<form>
-    <select name="sort" id="sort">
-        <option value="default">Squad 2E</option>
-        <option value="squad2h">Squad 2H</option>
-    </select>
-</form>
+<div class="sort-btns">
+    <form>
+        <!-- <select name="sort" id="sort"> -->
+            <option value="default">Squad 2E</option>
+            <option value="squad2h">Squad 2H</option>
+        <!-- </select> -->
+    </form>
+</div>
 
 {#each members as member}
-    <div class="profile-card">
-        <img src="{`https://fdnd.directus.app/assets/${member.mugshot}`}" width="200" alt="student-{member.name}">
-        <a href="/">{member.name}</a>
-    </div>
+    {#if member.mugshot}
+        <div class="profile-card">
+            <img src="{`https://fdnd.directus.app/assets/${member.mugshot}`}" width="220" alt="student-{member.name}">
+            <a href="/">{member.name}</a>
+        </div>
+    {/if}
 {/each}
