a6394819f9460f90362a27d67b5e973ee55422bd
Recep <recep.yildiz@hva.nl>
2025-09-14 23:04:21 +0200

    #29 Added breakpoints and styling
    Introduces responsive breakpoints with polished UI tweaks: better card sizing, spacing, and alignment for a more consistent look across viewports based on the design.

--- your-tribe-fl/src/routes/+page.svelte
+++ your-tribe-fl/src/routes/+page.svelte
@@ -3,24 +3,53 @@
   const members = data.members;
 </script>
 
-<section class="home-heading">
-    <h2>Meet the members</h2>
-</section>
+<div class="parent">
 
-<div class="sort-btns">
-    <form>
-        <!-- <select name="sort" id="sort"> -->
-            <option value="default">Squad 2E</option>
-            <option value="squad2h">Squad 2H</option>
-        <!-- </select> -->
-    </form>
-</div>
+    <!-- HEADING GRID -->
+    <div class="top-grid"> 
+        <section class="home-heading">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt.</p>
+            <h2>Meet the members</h2>
+        </section>
+    </div>
 
-{#each members as member}
-    {#if member.mugshot}
-        <div class="profile-card">
-            <img src="{`https://fdnd.directus.app/assets/${member.mugshot}`}" width="220" alt="student-{member.name}">
-            <a href="/">{member.name}</a>
+    <!-- BOTTOM LEFT GRID -->
+    <div class="left-grid">
+        <section class="spinner-container">
+            <h2>Placeholder for spinning text</h2>
+        </section>
+    </div>
+
+    <!-- BOTTOM RIGHT GRID -->
+    <div class="right-grid"> 
+
+        <div class="sort-btns">
+            <form>
+                <!-- <select name="sort" id="sort"> -->
+                    <option value="default">Squad 2E</option>
+                    <option value="squad2h">Squad 2H</option>
+                <!-- </select> -->
+            </form>
         </div>
-    {/if}
-{/each}
+
+        <div class="profile-container">
+            {#each members as member}
+                {#if member.mugshot}
+                <div class="profile-card">
+                    <img src="{`https://fdnd.directus.app/assets/${member.mugshot}`}" width="250" alt="student-{member.name}">
+                    <a href="/">{member.name}</a>
+                </div>
+                {/if}
+            {/each}
+        </div>
+
+    </div>
+
+</div> 
+
+
+
+
+
+
