4b5fb6b53ce65370b423706e6d4bea97a8364c9f
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-09-16 22:35:49 +0200

    header maken als component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <a href="/"><img src="assets/frame.svg" alt="logo van FDND" width="20px" height="15px"></a>
+        <button class="button-styling">Jaar 1</button>
+        <button class="button-styling">Jaar 2</button>
+    </nav>
+</header>
