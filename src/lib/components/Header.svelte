0ce8406cde184cc8b4adfabdf7a974c677a52758
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-09-17 15:02:05 +0200

    svg fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <header>
     <nav>
-        <a href="/"><img src="assets/frame.svg" alt="logo van FDND" width="20px" height="15px"></a>
+        <a href="/"><img src="assets/frame.svg" alt="logo van FDND"></a>
         <button class="button-styling">Jaar 1</button>
         <button class="button-styling">Jaar 2</button>
     </nav>
