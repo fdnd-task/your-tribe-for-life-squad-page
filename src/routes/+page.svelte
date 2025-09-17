ba29c2094b0c9f9ff5dccbd8dc8667b444b24162
SuleymanHG <suleyman.gokgul66@outlook.com>
2025-09-17 23:41:30 +0200

    #31 layout grid gefixt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,10 @@
-<h1>Welcome to SvelteKit!</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <script>
   export let data;
   const members = data?.members ?? [];
 </script>
 
- {#each members as m}
-      <img
-        src={`https://fdnd.directus.app/assets/${m.mugshot}?w=200&h=200&fit=cover`}
-        alt={m.name}
-      />
-{/each}
+<div class="layout-cards">
+    {#each members as m}
+      <img src={`https://fdnd.directus.app/assets/${m.mugshot}?w=200&h=200&fit=cover`} alt={m.name} />
+    {/each}
+</div>
