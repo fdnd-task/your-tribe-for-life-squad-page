703db76b6a12c2ce460e20829bcf81e909150ec9
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-09-18 22:36:47 +0200

    light/dark-mode button animation (#34)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,19 @@
 onMount(() => {
   const btn = document.getElementById("dark-mode-button")
 
+  if (btn) {
   btn.addEventListener("click", () => {
 
     document.body.classList.toggle("dark-mode-active")
+
+    btn.classList.remove("animation-shine")
+        void btn.offsetWidth 
+        btn.classList.add("animation-shine")
   })
+}
 })
 </script>
+
 
 
 <div class="layout-cards">
